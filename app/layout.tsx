cea59f46fdac23243bbd79c9372548ae21c0b37c
Marta B. <38409011+MartaBento@users.noreply.github.com>
2024-06-21 21:36:40 +0100

    feat: added new favicon and metadata setup; added eslint configuration from next.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,41 @@ export const metadata: Metadata = {
   title: "BTC Guessing Game",
   description:
     "A web app that allows users to make guesses on whether the market price of Bitcoin (BTC/USD) will be higher or lower after one minute.",
+  icons: {
+    icon: "/favicon.ico",
+    other: [
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        url: "/favicon-16x16.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        type: "image/png",
+        sizes: "180x180",
+        url: "/apple-touch-icon.png",
+      },
+      {
+        rel: "android-chrome",
+        url: "/android-chrome-192x192.png",
+        type: "image/png",
+        sizes: "192x192",
+      },
+      {
+        rel: "android-chrome",
+        url: "/android-chrome-512x512.png",
+        type: "image/png",
+        sizes: "512x512",
+      },
+    ],
+  },
 };
 
 type RootLayoutProps = {
